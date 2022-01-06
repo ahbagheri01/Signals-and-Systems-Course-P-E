209db51b15d62d5f0d4a635592b9b1acf1a0ae09
amirhoosein <ahbagheri01@gmail.com>
2022-01-06 23:28:56 +0330

    final

--- HW5/HW5.m
+++ HW5/HW5.m
@@ -81,22 +81,26 @@ syms n
 x1 = (heaviside(n+2)-heaviside(n-1))*(-n+1)-(heaviside(n-1) - heaviside(n-3))*(n+1);
 t1 = -5:5;
 step_fig(t1,subs(x1,t1),"plot");
-x1 = (heaviside(n)-heaviside(n-3))*(-n+3)+(heaviside(n-3) - heaviside(n-5))*(-n+1);
+x1 = (heaviside(n)-heaviside(n-3))*(-n+3)+(heaviside(n-3) - heaviside(n-5))*(-n+1)
 t1 = -5:10;
 step_fig(t1,subs(x1,t1),"plot");
 syms z
 H = z^{-2} * ztrans(x1) + 2*z^{-1}+3
+H = (3*z^2 + 2*z + 1-2*z^{-1}-3*z^{-2})/(z^2)
 plot_z_plan([3,2,1,-2,-3],[1,0,0])
-x2 = 0.8^n * heaviside(n-2);
+x2 = 0.8^n * heaviside(n-2)
 t2 = 0:15;
 step_fig(t2,subs(x2,t2),"plot")
 H = ztrans(x2)
 plot_z_plan([5/4,0],[5/4,-1,0,0])
-x3 = 2^n* cos(0.4*pi*n) * heaviside(n);
-t3 = -2:8;
+
+x3 = 2^n* cos(0.4*pi*n) * heaviside(n)
+t3 = -2:6;
 step_fig(t3,subs(x3,t3),"plot")
-H = ztrans(x2)
+H = ztrans(x3)
+H = (z-2*cos(0.4*pi))/(z^2-4*cos(0.4*pi)*z + 4)
 plot_z_plan([1,-2*cos(0.4*pi)],[1,-4*cos(0.4*pi),4])
+[r,p,k] = residuez([1,-2*cos(0.4*pi)],[1,-4*cos(0.4*pi),4])
 %% 
 % part two
 % 
@@ -119,6 +123,7 @@ plot_z_plan(num2,dom2)
 %% 
 % c
 
+oldparam = sympref('HeavisideAtOrigin',1);
 h1 = iztrans(H1)
 h2 = iztrans(H2)
 %% 
